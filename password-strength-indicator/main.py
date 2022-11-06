8ea718eeff6bd20702ae4c377ca17edb7fb6a284
0xlax <harshu8310@gmail.com>
2022-11-06 19:08:21 +0530

    added count function

--- password-strength-indicator/main.py
+++ password-strength-indicator/main.py
@@ -1,6 +1,9 @@
 import typer
 import getpass
 
+def leasteight(word):
+	if len(word) >= 8:
+		return True
 
 
 def main():
@@ -8,6 +11,9 @@ def main():
 	# password = input("Enter a password: ")
 	print("Password Considered!")
 	print(pswd)
+
+	if leasteight(pswd) == True:
+		print("passed string count")
 
 if __name__ == "__main__":
 	typer.run(main)
